2aca7042771f86b4b3743fa7c06525f26e73f901
Douw Muller <Douw.Muller@bcx.co.za>
2021-10-22 13:43:02 +0200

    '2021_10_22'

---
+++ Scripts/ProductEquivalence.sql
@@ -0,0 +1,10 @@
+SELECT * FROM cortex.productequivalencegenerics;
+SELECT * FROM cortex.productequivalenceatcs;
+SELECT * FROM cortex.productequivalencegenerictranslationactiveingredient;
+
+SELECT * FROM cortex.versionproductequivalenceatc;
+SELECT * FROM cortex.versionproductequivalencegeneric;
+
+/*reset*/
+SELECT * FROM cortex.abpbackgroundjobs;
+SELECT * FROM cortex.abpsettings;
